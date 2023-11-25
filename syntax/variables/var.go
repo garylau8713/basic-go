5c73f052a4d622fb11389de24a4cfc18ddc77596
Gary <gary@Garys-MacBook-Pro.local>
2023-11-24 20:44:56 -0800

    Init commit and upload for varianble lesson.

---
+++ syntax/variables/var.go
@@ -0,0 +1,25 @@
+package main
+
+// Package level var
+var (
+	a1 int = 123
+)
+
+func main() {
+	println(a1)
+
+	var a int = 456
+	println(a)
+
+	var b = 789
+	println(b)
+
+	var c = 213.4
+	println(c)
+
+	var str = "hello"
+	println(str)
+
+	var e int
+	println(e)
+}
